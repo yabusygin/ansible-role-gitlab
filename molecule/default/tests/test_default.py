c386196723459a9bbf3a0d6c97cfd2b03fe81299
Alexey Busygin <busygin.contact@yandex.ru>
2020-02-24 01:42:57 +0300

    Add default test scenario

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,15 +1,31 @@
+import json
 import os
 
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']
-).get_hosts('all')
+).get_hosts('instance')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
-
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+def test_health(host):
+    args = (
+        "http",
+        "--ignore-stdin",
+        "--check-status",
+        "--body",
+        "localhost/-/readiness?all=1",
+    )
+    cmd = host.run_expect(
+        expected=[0],
+        command=" ".join(args),
+    )
+    response = json.loads(s=cmd.stdout)
+    assert response["status"] == "ok"
+    assert response["cache_check"][0]["status"] == "ok"
+    assert response["db_check"][0]["status"] == "ok"
+    assert response["gitaly_check"][0]["status"] == "ok"
+    assert response["master_check"][0]["status"] == "ok"
+    assert response["queues_check"][0]["status"] == "ok"
+    assert response["redis_check"][0]["status"] == "ok"
+    assert response["shared_state_check"][0]["status"] == "ok"
