d69fddb954eb608e7ba51112d307539032d9fc35
Alexey Busygin <yaabusygin@gmail.com>
2022-08-24 22:44:12 +0300

    Do not mock yabusygin.docker role

--- lint.sh
+++ lint.sh
@@ -12,7 +12,9 @@ exit_handler() {
 trap exit_handler EXIT
 
 echo "Running ansible-lint..."
-ANSIBLE_COLLECTIONS_PATH="${MOLECULE_EPHEMERAL_DIRECTORY}/collections" ansible-lint \
+ANSIBLE_ROLES_PATH="${MOLECULE_EPHEMERAL_DIRECTORY}/roles" \
+ANSIBLE_COLLECTIONS_PATH="${MOLECULE_EPHEMERAL_DIRECTORY}/collections" \
+ansible-lint \
     "${MOLECULE_PROJECT_DIRECTORY}" \
     "${MOLECULE_SCENARIO_DIRECTORY}"
 
